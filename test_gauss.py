8ca4a12f0decc2cba94eb17f6789a44247c1ad07
d-shipley <d.shipley.1341@gmail.com>
2017-10-04 09:33:10 +0100

    Adding beginnings of a test file (forgot to add in previous commit

---
+++ test_gauss.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#   test_gauss.py
+#   
+#   This is a script to test the program gauss_int.py
+#
+
+import numpy as np
+import Q2-3_gaussian-quadrature.py as gq
+
+def test
